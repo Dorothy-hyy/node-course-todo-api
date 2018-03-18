d17aad76fd75b12893a3d2031364dc685d10e0b7
DorothyHYY <Doro_hyy@163.com>
2018-03-18 04:00:39 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectID} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -27,6 +29,21 @@ app.post('/todos',(req,res)=>{
         res.status(400).send(e);
     });
  });
+
+app.get('/todos/:id',(req, res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        res.status(404).send({});
+    }
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            res.status(404).send({});
+        }
+        res.status(200).send({todo});
+    },(e)=>{
+        res.status(400).send({});
+    });
+});
 
 app.listen(3000, ()=>{
     console.log('Started on port 3000');
