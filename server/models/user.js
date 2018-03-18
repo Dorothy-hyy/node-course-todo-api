6b50fd2cd6af06bcc6647c17fe87e27b7c850420
DorothyHYY <Doro_hyy@163.com>
2018-03-18 19:41:25 -0400

    Add GET /users/me and authenticate middleware

--- server/models/user.js
+++ server/models/user.js
@@ -34,6 +34,7 @@ var UserSchema = new mongoose.Schema({
     }]
 });
 
+//Seems like this one will automatically run??
 UserSchema.methods.toJSON = function(){
     var user = this;
     var userObject = user.toObject();
@@ -52,5 +53,23 @@ UserSchema.methods.generateAuthToken = function (){
     });
 };
 
+UserSchema.statics.findByToken = function(token){
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch(e){
+        // return new Promise((resolve, reject)=>{
+        //     reject();
+        // });
+        return Promise.reject();
+    }
+    return User.findOne({
+        _id: decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+};
 var User = mongoose.model('User', UserSchema);
 module.exports = {User};
